10fdd7db2fdf7fce9aabec39c8f806df644a5ba4
Lico-Tom <583566138@qq.com>
2023-03-02 16:43:00 +0800

    audit.

--- apollo-biz/src/main/java/com/ctrip/framework/apollo/biz/entity/Audit.java
+++ apollo-biz/src/main/java/com/ctrip/framework/apollo/biz/entity/Audit.java
@@ -26,8 +26,8 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "Audit")
-@SQLDelete(sql = "Update Audit set IsDeleted = true, DeletedAt = ROUND(UNIX_TIMESTAMP(NOW(4))*1000) where Id = ?")
+@Table(name = "`Audit`")
+@SQLDelete(sql = "Update \"Audit\" set IsDeleted = true, DeletedAt = ROUND(UNIX_TIMESTAMP(NOW(4))*1000) where Id = ?")
 @Where(clause = "isDeleted = false")
 public class Audit extends BaseEntity {
 
